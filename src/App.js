d604278513e29b094b2d0603c74954430379b106
Edward Zhuang <edward.zhuang@duke.edu>
2019-09-24 21:01:40 -0400

    Created static sign in page and registration page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from './components/Register';
+import Signin from './components/Signin';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function AppRouter() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route path="/" exact component={Index} />
+        <Route path="/signin/" component={Signin} />
+        <Route path="/register/" component={Register} />
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default AppRouter;
