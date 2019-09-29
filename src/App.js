23b9567b6020d4cfbada2ae42c02b5038bf24db8
Sam Chan <spc28@duke.edu>
2019-09-29 16:42:42 -0400

    Push up Edit Project page (to delete/update projects)

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Signin from "./components/Signin.js";
 import AllProjects from "./components/AllProjects.js";
 import Project from "./components/Project.js";
 import NewProject from "./components/NewProject.js";
+import EditProject from "./components/EditProject.js";
+import PresentProject from "./components/NewProject.js";
 import Home from "./components/Home.js";
 
 import "./App.css";
@@ -27,6 +29,8 @@ function AppRouter() {
           <Route path="/allprojects" component={AllProjects} />
           <Route path="/project" exact component={Project} />
           <Route path="/project/new" exact component={NewProject} />
+          <Route path="/project/edit" exact component={EditProject} />
+          <Route path="/project/present" exact component={PresentProject} />
           {/* <Route path="/artwork" exact component={Artwork} />
           <Route path="/artwork/new" exact component={NewArtwork} /> */}
           <Redirect exact from="/" to="/welcome" />
