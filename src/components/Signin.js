61d543a1ddb046315f7e86e748dcae3185e04a93
Edward Zhuang <edward.zhuang@duke.edu>
2019-10-10 19:11:36 -0400

    Added sign in logic

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,106 +1,187 @@
-import React from "react";
-import {
-  Avatar,
-  Button,
-  CssBaseline,
-  TextField,
-  Link,
-  Grid,
-  Box,
-  Typography,
-  makeStyles,
-  Container
-} from "@material-ui/core";
-import { LockOutlined as LockOutlinedIcon } from "@material-ui/icons";
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { withFirebase } from './Firebase';
 
-import Copyright from "./Copyright";
-import Navbar from "./Navbar";
 
-const useStyles = makeStyles(theme => ({
-  "@global": {
-    body: {
-      backgroundColor: theme.palette.common.white
-    }
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
+const SignInPage = () => (
+  <div>
+    <h1>Sign In</h1>
+    <SignInForm />
+    <p>
+      <Link to={'/register'}>Register</Link>
+    </p>
+  </div>
+);
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
   }
-}));
-
-export default function SignIn() {
-  const classes = useStyles();
-
-  return (
-    <React.Fragment>
-      <Navbar />
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link to="/register" variant="body2">
-                  {"Don't have an account? Register here!"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
-    </React.Fragment>
-  );
+  onSubmit = event => {
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push("/allprojects");
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  render() {
+    const { email, password, error } = this.state;
+    const isInvalid = password === '' || email === '';
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
 }
+const SignInForm = compose(
+  withRouter,
+  withFirebase,
+)(SignInFormBase);
+
+export default SignInPage;
+export { SignInForm };
+
+// import {
+//   Avatar,
+//   Button,
+//   CssBaseline,
+//   TextField,
+//   Link,
+//   Grid,
+//   Box,
+//   Typography,
+//   makeStyles,
+//   Container
+// } from "@material-ui/core";
+// import { LockOutlined as LockOutlinedIcon } from "@material-ui/icons";
+
+// import Copyright from "./Copyright";
+// import Navbar from "./Navbar";
+
+// const useStyles = makeStyles(theme => ({
+//   "@global": {
+//     body: {
+//       backgroundColor: theme.palette.common.white
+//     }
+//   },
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center"
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main
+//   },
+//   form: {
+//     width: "100%", // Fix IE 11 issue.
+//     marginTop: theme.spacing(1)
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2)
+//   }
+// }));
+
+
+
+
+
+// export default function SignIn() {
+//   const classes = useStyles();
+
+//   return (
+//     <React.Fragment>
+//       <Navbar />
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <div className={classes.paper}>
+//           <Avatar className={classes.avatar}>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h5">
+//             Sign in
+//           </Typography>
+//           <form className={classes.form} noValidate>
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="username"
+//               label="Username"
+//               name="username"
+//               autoComplete="username"
+//               autoFocus
+//             />
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               name="password"
+//               label="Password"
+//               type="password"
+//               id="password"
+//               autoComplete="current-password"
+//             />
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               className={classes.submit}
+//             >
+//               Sign In
+//             </Button>
+//             <Grid container>
+//               <Grid item>
+//                 <Link to="/register" variant="body2">
+//                   {"Don't have an account? Register here!"}
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </form>
+//         </div>
+//         <Box mt={8}>
+//           <Copyright />
+//         </Box>
+//       </Container>
+//     </React.Fragment>
+//   );
+// }
