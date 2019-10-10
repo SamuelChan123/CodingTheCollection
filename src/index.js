c907283fbdd64300cb57b551140137179d4253a0
Edward Zhuang <edward.zhuang@duke.edu>
2019-10-10 17:40:33 -0400

    Added eslint and began development of firebase directory

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppRouter from "./App";
+import Firebase, { FirebaseContext } from './components/Firebase'
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <AppRouter />
+    </FirebaseContext.Provider>, 
+    document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
