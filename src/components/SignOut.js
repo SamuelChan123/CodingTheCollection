51c81f413b1252b5453956239d36e8e06ee6995a
Edward Zhuang <edward.zhuang@duke.edu>
2019-10-21 12:02:16 -0400

    Stylize sign out button

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { withFirebase } from './Firebase';
+import { Button } from '@material-ui/core';
 
 const SignOutButton = ({ firebase }) => (
-  <button color="inherit" type="button" onClick={firebase.doSignOut}>
+  <Button color="inherit" type="button" onClick={firebase.doSignOut}>
     Sign Out
-  </button>
+  </Button>
 );
 
 export default withFirebase(SignOutButton);
